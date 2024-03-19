bf30d8b03158c63c44156cd059f887f62b9a6b47
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2024-03-19 17:59:45 +0100

    project: folder structure changed; trigger_backup script improved

--- philosophie.ch_legacy/sysadmin/trigger_backup.sh
+++ philosophie.ch_legacy/sysadmin/trigger_backup.sh
@@ -24,29 +24,49 @@ case "${1}" in
 esac
 
 
+## SET THE CORRECT DIRECTORIES HERE
+rails_dir="/home/sysadmin/philosophiechlegacy"
+db_dir="/home/sysadmin/philosophiechlegacy-db/data"
+##
+
+echo "Backing up philosophie.ch legacy data to Infomaniak's Swiss Backup..."
+
+echo "Setting up backup directories..."
 ts=$( date +%Y%m%d )
 base_dir="/home/sysadmin/backups"
-
 bak_dir="${base_dir}/${ts}"
 mkdir -p "${bak_dir}"
 mkdir -p "${bak_dir}/public"
-
+mkdir -p "${db_dir}"
+echo "Backup directories set up."
 
 # Backup rails assets
-rails_dir="/home/sysadmin/philosophiechlegacy"
-
+echo "Backing up rails assets..."
+echo "Assets: ${bak_dir}/storage"
 cp -r "${rails_dir}/storage" "${bak_dir}"
+echo "Assets: ${bak_dir}/uploads"
 cp -r "${rails_dir}/uploads" "${bak_dir}"
+echo "Assets: ${bak_dir}/public/system"
 cp -r "${rails_dir}/public/system" "${bak_dir}/public/system"
+echo "Assets: ${bak_dir}/public/pictures"
 cp -r "${rails_dir}/public/pictures" "${bak_dir}/public/pictures"
+echo "Backing up rails assets done."
 
 # Backup database
+echo "Backing up database..."
 docker exec philosophiechlegacy-db /bin/bash -c "pg_dump \"postgresql://\${POSTGRES_USER}:\${POSTGRES_PASSWORD}@127.0.0.1/\${POSTGRES_DB}\" --column-inserts --no-owner --no-privileges > /var/lib/postgresql/data/backup.dump"
+sudo cp "${db_dir}/backup.dump" "${bak_dir}"
+echo "Backing up database done."
 
-db_dir="/home/sysadmin/philosophiechlegacy-db/data"
-cp "${db_dir}/backup.dump" "${bak_dir}"
+
+# Set up permissions
+echo "Setting up permissions..."
+sudo chown sysadmin:sysadmin "${bak_dir}"
+echo "Permissions set up."
 
 
 # Configured to use Infomaniak's Swiss Backup
+echo "Syncing backup to Infomaniak's Swiss Backup..."
 rclone sync /home/sysadmin/backups sb_project_SBI-LB170495:philosophie.ch/backups
+echo "Syncing backup to Infomaniak's Swiss Backup done."
 
