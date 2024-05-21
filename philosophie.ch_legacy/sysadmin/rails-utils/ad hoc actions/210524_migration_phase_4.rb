986c3796f94a1eeaab5cfaeefa0694de9b941a53
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2024-05-21 13:02:01 +0200

    Try: migration phase 4 beta for all pages required

--- philosophie.ch_legacy/sysadmin/rails-utils/ad hoc actions/210524_migration_phase_4.rb
+++ philosophie.ch_legacy/sysadmin/rails-utils/ad hoc actions/210524_migration_phase_4.rb
@@ -2,6 +2,8 @@ require 'csv'
 
 rp = Alchemy::Page.find(5098)
 
+
+# Main loop for single pages
 mismatched_slugs_single_pages = []
 errors_single_pages = []
 
@@ -9,7 +11,7 @@ CSV.foreach('210524_migration_phase_4_single_pages.csv', col_sep: ',', headers: 
   l = row[0]  
   slug_raw = row[1]
   id = row[2]
-  tags = row[3..-1].map(&:strip)
+  tags = row[3..-1]
   page = Alchemy::Page.find(id)
   ids_match = page.urlname == slug_raw
 
@@ -39,3 +41,45 @@ puts "\n\n***Single pages report***\n\n
 ==> Mismatched slugs:\n#{mismatched_slugs_single_pages.join("\n")}\n\n
 ==> Errors:\n#{errors_single_pages.join("\n")}\n\n
 ***End of single pages report***\n\n"
+
+
+# Main loop for parents
+mismatched_slugs_parents = []
+errors_parents = []
+
+CSV.foreach('210524_migration_phase_4_parents.csv', col_sep: ',', headers: false) do |row|
+  l = row[0]  
+  slug_raw = row[1]
+  id = row[2]
+  tags = row[3..-1]
+  page = Alchemy::Page.find(id)
+  ids_match = page.urlname == slug_raw
+
+  if !ids_match
+    mismatched_slugs_parents << [id, slug_raw, page.urlname]
+    next
+  end
+
+  begin
+    if page.parent_id != rp.id
+      puts "Dry moving all children of page #{id} to #{rp.id}"
+      #page.children.each do |child|
+      #  child.tag_names = tags
+      #  child.language_id = rp.language_id
+      #  child.language_code = rp.language_code
+      #  child.parent_id = rp.id
+      #  child.save
+    else
+      errors_parents << [id, slug_raw, "Page already moved"]
+    end
+  rescue => e
+    errors_parents << [id, slug_raw, e.message]
+    next
+  end 
+end
+
+puts "\n\n***Parents report***\n\n
+==> Mismatched slugs:\n#{mismatched_slugs_parents.join("\n")}\n\n
+==> Errors:\n#{errors_parents.join("\n")}\n\n
+***End of parents report***\n\n"
+
