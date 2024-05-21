ee3eebec238a9a4d1fd5cc294c0a4a6589c6802a
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2024-05-21 14:00:05 +0200

    Try: migration phase 4 pt 2

--- philosophie.ch_legacy/sysadmin/rails-utils/ad hoc actions/210524_migration_phase_4.rb
+++ philosophie.ch_legacy/sysadmin/rails-utils/ad hoc actions/210524_migration_phase_4.rb
@@ -14,7 +14,7 @@ moved_single_pages = []
 mismatched_slugs_single_pages = []
 errors_single_pages = []
 
-CSV.foreach('210524_migration_phase_4_single_pages.csv', col_sep: ',', headers: false) do |row|
+CSV.foreach('210524_migration_phase_4_single_pages_pt2.csv', col_sep: ',', headers: false) do |row|
   l = row[0]  
   slug_raw = row[1]
   id = row[2]
@@ -22,67 +22,65 @@ CSV.foreach('210524_migration_phase_4_single_pages.csv', col_sep: ',', headers: 
   page = Alchemy::Page.find(id)
   ids_match = page.urlname == slug_raw
 
-  if !ids_match
-    mismatched_slugs_single_pages << [id, slug_raw, page.urlname]
-    next
-  end
-
-  begin
-    if page.parent_id != rp.id
-      #puts "Dry moving page #{id} to #{rp.id}"
-      page.tag_names = tags
-      page.language_id = rp.language_id
-      page.language_code = rp.language_code
-      page.parent_id = rp.id
-      page.save
-      moved_single_pages << [id, slug_raw]
-    else
-      errors_single_pages << [id, slug_raw, "Page already moved"]
-    end
-  rescue => e
-    errors_single_pages << [id, slug_raw, e.message]
-    next
-  end 
-end
-
-
-# Main loop for parents
-moved_parents = []
-mismatched_slugs_parents = []
-errors_parents = []
-
-CSV.foreach('210524_migration_phase_4_parents.csv', col_sep: ',', headers: false) do |row|
-  l = row[0]  
-  slug_raw = row[1]
-  id = row[2]
-  tags = row[3..-1]
-  page = Alchemy::Page.find(id)
-  ids_match = page.urlname == slug_raw
-
-  if !ids_match
-    mismatched_slugs_parents << [id, slug_raw, page.urlname]
-    next
-  end
-
-  begin
-    if page.parent_id != rp.id
-      #puts "Dry moving all children of page #{id} to #{rp.id}"
-      page.children.each do |child|
-        child.tag_names = tags
-        child.language_id = rp.language_id
-        child.language_code = rp.language_code
-        child.parent_id = rp.id
-        child.save
+  if ids_match
+    begin
+      if page.parent_id != rp.id
+        page.tag_names = tags
+        page.language_id = rp.language_id
+        page.language_code = rp.language_code
+        page.parent_id = rp.id
+        page.save
+        moved_single_pages << [id, slug_raw]
+      else
+        errors_single_pages << [id, slug_raw, "Page already moved"]
       end
-      moved_parents << [id, slug_raw]
-    else
-      errors_parents << [id, slug_raw, "Page already moved"]
-    end
-  rescue => e
-    errors_parents << [id, slug_raw, e.message]
-    next
-  end 
+    rescue => e
+      errors_single_pages << [id, slug_raw, e.message]
+    end 
+
+  else
+    mismatched_slugs_single_pages << [id, slug_raw, page.urlname]
+  end
+
 end
+
+
+## Main loop for parents
+#moved_parents = []
+#mismatched_slugs_parents = []
+#errors_parents = []
+
+#CSV.foreach('210524_migration_phase_4_parents.csv', col_sep: ',', headers: false) do |row|
+  #l = row[0]  
+  #slug_raw = row[1]
+  #id = row[2]
+  #tags = row[3..-1]
+  #page = Alchemy::Page.find(id)
+  #ids_match = page.urlname == slug_raw
+
+  #if ids_match
+    #begin
+      #if page.parent_id != rp.id
+        ##puts "Dry moving all children of page #{id} to #{rp.id}"
+        #page.children.each do |child|
+          #child.tag_names = tags
+          #child.language_id = rp.language_id
+          #child.language_code = rp.language_code
+          #child.parent_id = rp.id
+          #child.save
+        #end
+        #moved_parents << [id, slug_raw]
+      #else
+        #errors_parents << [id, slug_raw, "Page already moved"]
+      #end
+    #rescue => e
+      #errors_parents << [id, slug_raw, e.message]
+    #end 
+
+  #else
+    #mismatched_slugs_parents << [id, slug_raw, page.urlname]
+  #end
+#end
 
 
 puts "\n\n***Single pages report***\n\n
@@ -92,9 +90,9 @@ puts "\n\n***Single pages report***\n\n
 ***End of single pages report***\n\n"
 
 
-puts "\n\n***Parents report***\n\n
-==> Moved pages:\n#{moved_parents.join("\n")}\n\n
-==> Mismatched slugs:\n#{mismatched_slugs_parents.join("\n")}\n\n
-==> Errors:\n#{errors_parents.join("\n")}\n\n
-***End of parents report***\n\n"
+#puts "\n\n***Parents report***\n\n
+#==> Moved pages:\n#{moved_parents.join("\n")}\n\n
+#==> Mismatched slugs:\n#{mismatched_slugs_parents.join("\n")}\n\n
+#==> Errors:\n#{errors_parents.join("\n")}\n\n
+#***End of parents report***\n\n"
 
