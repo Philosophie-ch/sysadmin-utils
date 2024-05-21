cb17a30b31fb0b63806f8ecf09d04c68e8b52f1a
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2024-05-21 12:54:53 +0200

    Try: migration phase 4 beta version

--- philosophie.ch_legacy/sysadmin/rails-utils/ad hoc actions/210524_migration_phase_4.rb
+++ philosophie.ch_legacy/sysadmin/rails-utils/ad hoc actions/210524_migration_phase_4.rb
@@ -2,6 +2,9 @@ require 'csv'
 
 rp = Alchemy::Page.find(5098)
 
+mismatched_slugs_single_pages = []
+errors_single_pages = []
+
 CSV.foreach('210524_migration_phase_4_single_pages.csv', col_sep: ',', headers: false) do |row|
   l = row[0]  
   slug_raw = row[1]
@@ -9,5 +12,30 @@ CSV.foreach('210524_migration_phase_4_single_pages.csv', col_sep: ',', headers: 
   tags = row[3..-1].map(&:strip)
   page = Alchemy::Page.find(id)
   ids_match = page.urlname == slug_raw
-  puts "Page with id #{id} and urlname #{slug_raw} is matching? #{ids_match}"
+
+  if !ids_match
+    mismatched_slugs_single_pages << [id, slug_raw, page.urlname]
+    next
+  end
+
+  begin
+    if page.parent_id != rp.id
+      puts "Dry moving page #{id} to #{rp.id}"
+      #page.tag_names = tags
+      #page.language_id = rp.language_id
+      #page.language_code = rp.language_code
+      #page.parent_id = rp.id
+      #page.save
+    else
+      errors_single_pages << [id, slug_raw, "Page already moved"]
+    end
+  rescue => e
+    errors_single_pages << [id, slug_raw, e.message]
+    next
+  end 
 end
+
+puts "\n\n***Single pages report***\n\n
+==> Mismatched slugs:\n#{mismatched_slugs_single_pages.join("\n")}\n\n
+==> Errors:\n#{errors_single_pages.join("\n")}\n\n
+***End of single pages report***\n\n"
