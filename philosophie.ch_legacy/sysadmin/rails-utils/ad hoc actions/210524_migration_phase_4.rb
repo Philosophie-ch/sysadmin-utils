d1ab81e735047fe7b7dbfa186df7e5d5d12b7cc4
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2024-05-21 12:09:40 +0200

    try: prototype for migration phase 4

---
+++ philosophie.ch_legacy/sysadmin/rails-utils/ad hoc actions/210524_migration_phase_4.rb
@@ -0,0 +1,11 @@
+require 'csv'
+
+rp = Alchemy::Page.find(5098)
+
+CSV.foreach('210524_migration_phase_4_single_pages.csv', col_sep: ',', headers: false) do |row|
+  slug_raw = row[0]
+  id = row[1]
+  tags = row[2:row.size].map(&:strip)
+  page = Alchemy::Page.find(id)
+  ids_match = page.urlname == slug_raw
+  puts "Page with id #{id} and urlname #{slug_raw} is matching? #{ids_match}"
