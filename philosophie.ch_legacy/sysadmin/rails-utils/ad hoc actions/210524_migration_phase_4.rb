4ead94fea47bf25d0d8b73178f48994ed337ca20
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2024-05-21 12:38:41 +0200

    fix: typos and parsing

--- philosophie.ch_legacy/sysadmin/rails-utils/ad hoc actions/210524_migration_phase_4.rb
+++ philosophie.ch_legacy/sysadmin/rails-utils/ad hoc actions/210524_migration_phase_4.rb
@@ -3,9 +3,10 @@ require 'csv'
 rp = Alchemy::Page.find(5098)
 
 CSV.foreach('210524_migration_phase_4_single_pages.csv', col_sep: ',', headers: false) do |row|
-  slug_raw = row[0]
-  id = row[1]
-  tags = row[2:row.size].map(&:strip)
+  l = row[0]  
+  slug_raw = row[1]
+  id = row[2]
+  tags = row[3..-1].map(&:strip)
   page = Alchemy::Page.find(id)
   ids_match = page.urlname == slug_raw
   puts "Page with id #{id} and urlname #{slug_raw} is matching? #{ids_match}"
