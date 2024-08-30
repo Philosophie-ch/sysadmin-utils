d02a78871755fb7a9d3b5dbc54eeb70356fc5afd
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2024-08-30 18:02:05 +0200

    feat: new events_tasks script

---
+++ philosophie.ch_legacy/sysadmin/rails-utils/events_tasks.rb
@@ -0,0 +1,261 @@
+require 'csv'
+
+ActiveRecord::Base.logger.level = Logger::WARN
+
+Rails.logger.level = Logger::INFO
+
+if ARGV[0]
+  # set log level
+  if ARGV[0] == 'debug'
+    Rails.logger.level = Logger::DEBUG
+  elsif ARGV[0] == 'info'
+    Rails.logger.level = Logger::INFO
+  elsif ARGV[0] == 'warn'
+    Rails.logger.level = Logger::WARN
+  elsif ARGV[0] == 'error'
+    Rails.logger.level = Logger::ERROR
+  end
+end
+
+
+def generate_csv_report(report)
+  return if report.empty?
+  headers = report.first.keys
+
+  csv_string = CSV.generate do |csv|
+    csv << headers
+    report.each do |row|
+      csv << headers.map { |header| row[header] }
+    end
+  end
+
+  file_name = "#{Time.now.strftime('%y%m%d')}_events_tasks_report.csv"
+  File.write(file_name, csv_string)
+
+  Rails.logger.info("\n\n\n============ Report generated at #{file_name} ============\n\n\n")
+end
+
+
+############
+# MAIN
+############
+
+report = []
+CSV.foreach("events_tasks.csv", col_sep: ',', headers: true) do |row|
+
+  subreport = {
+    _request: row["_request"] || '',
+    id: row["id"] || '',
+    date: row["date"] || '',
+    profile_slug: row["profile_slug"] || '',
+    title: row["title"] || '',
+    city: row["city"] || '',
+    _old_link: row["_old_link"] || '',
+    link: row["link"] || '',
+    _recurrent: row["_recurrent"] || '',
+    _to_do_for_us: row["_to_do_for_us"] || '',
+
+    status: '',
+    changes_made: '',
+    error_message: ''
+  }
+
+
+  begin
+
+    # Control
+    Rails.logger.info("Starting task for event...")
+
+    supported_requests = ['POST', 'UPDATE', 'GET', 'DELETE']
+    req = subreport[:_request]
+
+    if req.nil? || req.empty? || req.strip == ''
+      subreport[:status] = ''
+    else
+      unless supported_requests.include?(req)
+        subreport[:status] = 'error'
+        subreport[:error_message] = "Request not supported: #{req}. Skipping..."
+      end
+    end
+
+    if req == 'POST'
+      same_events = Event.where(title: subreport[:title], city: subreport[:city], date: subreport[:date])
+      unless same_events.empty?
+        subreport[:status] = 'error'
+        subreport[:error_message] = "Event with the same title, city, and date already exists. Skipping..."
+        next
+      end
+    end
+
+    if req == 'UPDATE' || req == 'POST'
+      if subreport[:title].nil? || subreport[:title].empty? || subreport[:title].strip == '' || subreport[:title].blank?
+        subreport[:status] = 'error'
+        subreport[:error_message] = "Title is empty, but required for '#{req}'. Skipping..."
+        next
+      end
+
+      if subreport[:city].nil? || subreport[:city].empty? || subreport[:city].strip == '' || subreport[:city].blank?
+        subreport[:status] = 'error'
+        subreport[:error_message] = "City is empty, but required for '#{req}'. Skipping..."
+        next
+      end
+
+      if subreport[:date].nil? || subreport[:date].empty? || subreport[:date].strip == '' || subreport[:date].blank?
+        subreport[:status] = 'error'
+        subreport[:error_message] = "Date is empty, but required for '#{req}'. Skipping..."
+        next
+      end
+
+      if subreport[:profile_slug].nil? || subreport[:profile_slug].empty? || subreport[:profile_slug].strip == '' || subreport[:profile_slug].blank?
+        subreport[:status] = 'error'
+        subreport[:error_message] = "Profile slug is empty, but required for '#{req}'. Skipping..."
+        next
+      else
+        if Profile.find_by(slug: subreport[:profile_slug]).nil?
+          subreport[:status] = 'error'
+          subreport[:error_message] = "Profile with slug '#{subreport[:profile_slug]}' not found. Skipping..."
+          next
+        end
+      end
+
+    end
+
+    # Parsing
+    Rails.logger.info("Parsing task for event...")
+    id = subreport[:id]
+    date = subreport[:date]
+    profile_slug = subreport[:profile_slug]
+    title = subreport[:title]
+    city = subreport[:city]
+    link = subreport[:link]
+
+
+    # Setup
+    Rails.logger.info("Processing event...")
+
+    if req == 'POST'
+      event = Event.new
+
+    elsif req == 'UPDATE' || req == 'DELETE' || req == 'GET'
+
+      unless id.nil? || id.empty? || id.strip == '' || id.blank?
+        # try to retrieve event by ID if there's one
+        event = Event.find_by(id: id)
+
+        if event.nil?
+          Rails.logger.error("Event with ID #{id} not found. Skipping...")
+          subreport[:status] = 'error'
+          subreport[:error_message] = "Event with ID #{id} not found. Skipping..."
+          next
+        end
+
+      else
+        # else try to retrieve event by title, city, and date
+        event = Event.where(title: title, city: city, date: date).first
+        retrieved_profile_slug = event.profile.slug
+
+        if subreport[:profile_slug] != retrieved_profile_slug
+          subreport[:status] = 'error'
+          subreport[:error_message] = "Profile slug mismatch: '#{subreport[:profile_slug]}' != '#{retrieved_profile_slug}'. Skipping..."
+          next
+        end
+      end
+
+
+    else  # should not happen
+      Rails.logger.error("Request not supported: #{req}. Skipping...")
+      subreport[:status] = 'error'
+      subreport[:error_message] = "Request not supported: #{req}. Skipping..."
+      next
+    end
+
+    if req == 'DELETE'
+      event.delete
+      if Event.find_by(id: id).exists?
+        subreport[:status] = 'error'
+        subreport[:error_message] = "Event with ID #{id} not deleted for an unknown reason!"
+        next
+      else
+        subreport[:id] = 'EVENT WAS DELETED IN THE SERVER'
+        subreport[:status] = 'success'
+        next
+      end
+    end
+
+    if req == 'UPDATE' || req == 'GET'
+      old_event = {
+        _request: subreport[:_request],
+        id: event.id,
+        date: event.date.strftime('%Y-%m-%d'),
+        profile_slug: event.profile.slug,
+        title: event.title,
+        city: event.city,
+        _old_link: subreport[:_old_link],
+        link: event.link,
+        _recurrent: subreport[:_recurrent],
+        _to_do_for_us: subreport[:_to_do_for_us],
+
+        status: subreport[:status],
+        changes_made: subreport[:changes_made],
+        error_message: subreport[:error_message]
+      }
+    end
+
+    # Execution
+    Rails.logger.info("Executing task for event...")
+
+    if req == 'POST' || req == 'UPDATE'
+      Rails.logger.info("Processing event: setting attributes...")
+      event.date = date
+      event.profile = Profile.find_by(slug: profile_slug)
+      event.title = title
+      event.city = city
+      event.link = link
+
+      successful_save = event.save!
+      if successful_save
+        subreport[:status] = 'success'
+      else
+        subreport[:status] = 'error'
+        subreport[:error_message] = "Event not saved for an unknown reason! Skipping..."
+        next
+      end
+    end
+
+    # Update report
+    Rails.logger.info("Updating report...")
+    subreport.merge!({
+      id: event.id,
+      date: event.date.strftime('%Y-%m-%d'),
+      profile_slug: event.profile.slug,
+      title: event.title,
+      city: event.city,
+      link: event.link
+    })
+
+    if req == 'UPDATE' || req == 'GET'
+      changes = []
+      subreport.each do |key, value|
+        if old_event[key] != value && key != :changes_made && key != :status && key != :error_message
+          unless old_event[key].to_s.empty? && value.to_s.empty?
+            changes << "#{key}: #{old_event[key]} => #{value}"
+          end
+        end
+      end
+      subreport[:changes_made] = changes.join(' ;;; ')
+    end
+
+    subreport[:status] = 'success'
+    Rails.logger.info("Event processed successfully!")
+
+  rescue StandardError => e
+    subreport[:status] = 'unexpected error'
+    subreport[:error_message] = e.message
+
+  ensure
+    report << subreport
+  end
+
+end
+
+generate_csv_report(report)
