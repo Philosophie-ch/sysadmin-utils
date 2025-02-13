8e92178ab18989ac8e66905c03b5741ca744a8dc
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2025-02-13 13:45:33 +0100

    feat(ad hoc tasks): script to clean html tags of unwanted formatting

---
+++ philosophie.ch_legacy/sysadmin/rails-utils/ad hoc actions/250213_clean_html_tags.rb
@@ -0,0 +1,154 @@
+require 'csv'
+
+def clean_unused_html_tags(html)
+
+  return "" if html.nil?
+
+  html.gsub(/text-align:\s*justify;?/i, "")
+      .gsub(/align\s*=\s*["']?justify["']?;?/i, "")
+      .gsub(/font-size:[^;]*;/, "")
+      .gsub(/font-family:[^;]*;/, "")
+      .gsub(/line-height:[^;]*;/, "")
+      .gsub(/color:[^;]*;/, "")
+      .gsub(/margin-[^:]+:[^;]*;/, "")
+      .gsub(/lang="[^"]*"/, "")
+      .gsub("  ", " ")
+      .gsub(/style=" "/, "")
+      .gsub(/style=""/, "")
+      .gsub("  ", " ")
+end
+
+all_pages = Alchemy::Page.all
+
+report = []
+
+all_pages.each do |page|
+
+  page.elements.each do |element|
+
+      case element.name
+
+      when 'intro', 'text_block', 'text_and_picture'
+
+        richtext_contents = element.contents.where(name: ['pre_headline', 'lead_text', 'text'])
+
+
+        richtext_contents.each do |content|
+          essence = content.essence
+
+          essence_report = {
+            page_id: page.id,
+            page_slug: page.urlname,
+            element_id: element.id,
+            element_name: element.name,
+            content_id: content.id,
+            content_name: content.name,
+            essence_id: essence.id,
+            essence_type: essence.class.name,
+            essence_old_body: essence.body,
+            essence_new_body: "",
+            html_cleanup_status: "",
+            unexpected_error: "",
+          }
+
+          begin
+
+            cleaned_body = clean_unused_html_tags(essence.body)
+            if cleaned_body != essence.body
+              essence.body = cleaned_body
+              essence.save!
+              essence_report[:essence_new_body] = essence.body
+              essence_report[:html_cleanup_status] = "success"
+            else
+              essence_report[:html_cleanup_status] = "skipped"
+              essence_report[:unexpected_error] = "Cleanup produced no changes"
+            end
+
+            page.save!
+            page.publish!
+
+          rescue => e
+            essence_report[:unexpected_error] = "#{e.class} - #{e.message} - #{e.backtrace.join(" ::: ")}"
+          ensure
+            report << essence_report
+          end
+        end
+
+      when 'aside_column'
+        # This one has nested elements
+        element.elements.each do |nested_element|
+          if ["text_block", "text_and_picture"].include?(nested_element.name)
+            nested_element.contents.each do |content|
+              essence = content.essence
+
+              essence_report = {
+                page_id: page.id,
+                page_slug: page.urlname,
+                element_id: element.id,
+                element_name: element.name,
+                content_id: content.id,
+                content_name: content.name,
+                essence_id: essence.id,
+                essence_type: essence.type,
+                essence_old_body: essence.body,
+                essence_new_body: "",
+                html_cleanup_status: "",
+                unexpected_error: "",
+              }
+
+              begin
+
+                cleaned_body = clean_unused_html_tags(essence.body)
+                if cleaned_body != essence.body
+                  essence.body = cleaned_body
+                  essence.save!
+                  essence_report[:essence_new_body] = essence.body
+                  essence_report[:html_cleanup_status] = "success"
+                else
+                  essence_report[:html_cleanup_status] = "skipped"
+                  essence_report[:unexpected_error] = "Cleanup produced no changes"
+                end
+
+                  page.save!
+                  page.publish!
+
+              rescue => e
+                essence_report[:unexpected_error] = "#{e.class} - #{e.message} - #{e.backtrace.join(" ::: ")}"
+              ensure
+                report << essence_report
+              end
+            end
+          end
+        end
+
+      else
+        report << {
+          page_id: page.id,
+          page_slug: page.urlname,
+          element_id: element.id,
+          element_name: element.name,
+          content_id: "",
+          content_name: "",
+          essence_id: "",
+          essence_type: "",
+          essence_old_body: "",
+          essence_new_body: "",
+          html_cleanup_status: "skipped",
+          unexpected_error: "",
+        }
+
+    end
+  end
+end
+
+
+# Write report to CSV
+base_folder = "portal-tasks-reports"
+FileUtils.mkdir_p(base_folder) unless File.directory?(base_folder)
+
+CSV.open("#{base_folder}/250213_html_cleanup_report.csv", "wb") do |csv|
+  csv << report.first.keys
+  report.each do |row|
+    csv << row.values
+  end
+end
