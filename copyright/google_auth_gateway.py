dac1a923ff08eca2d9d43982ce2aa21fe00e8ee7
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2025-04-03 17:13:41 +0200

    feat(copyright): implemented code to use google vision api, google CSE, and local watermark check
    - Experiment with Selenium code, but it's hard to make it work; may replace it with Playwright

---
+++ copyright/google_auth_gateway.py
@@ -0,0 +1,40 @@
+import os
+
+def authenticate_google(json_key_filepath: str) -> None:
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json_key_filepath
+
+
+def main(
+    json_key_filepath: str,
+) -> None:
+    """
+    Authenticate Google Cloud using a service account key file.
+
+    Args:
+        json_key_filepath (str): Path to the service account key file.
+    """
+    authenticate_google(json_key_filepath)
+
+
+def cli() -> None:
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Authenticate Google Cloud using a service account key file.")
+
+    parser.add_argument(
+        "-j",
+        "--json-key-filepath",
+        type=str,
+        required=True,
+        help="Path to the service account key file.",
+    )
+
+    args = parser.parse_args()
+
+    main(
+        json_key_filepath=args.json_key_filepath,
+    )
+
+
+if __name__ == "__main__":
+    cli()
