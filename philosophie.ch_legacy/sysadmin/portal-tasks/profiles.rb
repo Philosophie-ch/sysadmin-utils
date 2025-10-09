4e82e11c215fb93dbc56066302376586fa5051db
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2025-10-09 19:52:24 +0200

    feat(profiles): rename _temp to _sort in subreport for clarity and consistency

--- philosophie.ch_legacy/sysadmin/portal-tasks/profiles.rb
+++ philosophie.ch_legacy/sysadmin/portal-tasks/profiles.rb
@@ -47,7 +47,7 @@ def main(csv_file, log_level = 'info')
     Rails.logger.info("Processing row #{processed_lines + 1}... of #{total_lines}")
 
     subreport = {
-      _temp: row["_temp"] || "",
+      _sort: row["_sort"] || "",
       _correspondence: row["_correspondence"] || "",
       _todo_person: row["_todo_person"] || "",
       _request: row["_request"] || "",
@@ -328,7 +328,7 @@ def main(csv_file, log_level = 'info')
         old_biblio_further_asset_url = user.profile.bibliography_further_references_asset_url.blank? ? "" : user.profile.bibliography_further_references_asset_url.gsub(bibliography_base_url, '')
 
         old_user = {
-          _temp: subreport[:_temp],
+          _sort: subreport[:_sort],
           _correspondence: subreport[:_correspondence],
           _todo_person: subreport[:_todo_person],
           _request: subreport[:_request],
