3f662d0cf3db73f2b9922ab8e354ec4316116d00
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2026-05-13 14:59:23 +0200

    fix: simplify news script back to just 4 columns

--- philosophie.ch_legacy/sysadmin/portal-tasks/news.rb
+++ philosophie.ch_legacy/sysadmin/portal-tasks/news.rb
@@ -40,48 +40,12 @@ def main(log_level = 'info')
   event_ids = Event.pluck(:id)
   topic_ids = Topic.pluck(:id)
 
-  # Fetch non-biblio profile data with last_updated info using SQL joins
-  # Only for non-biblio profiles (fast and efficient!)
-  profile_only_data = Alchemy::User
-    .where("alchemy_users.alchemy_roles NOT LIKE ?", "%biblio%")
-    .joins("LEFT JOIN alchemy_users AS updaters ON alchemy_users.updater_id = updaters.id")
-    .select("alchemy_users.id, updaters.login AS updater_login, alchemy_users.updated_at")
-    .map do |user|
-      {
-        id: user.id,
-        updater_login: user.updater_login || '',
-        updated_at: user.updated_at&.strftime('%Y-%m-%d') || ''
-      }
-    end
-
-  # Fetch page data with creator, updater, last_updated_date, and replied_by info using SQL joins
-  # Note: Using STRING_AGG for PostgreSQL (GROUP_CONCAT is MySQL syntax)
-  # Removed DISTINCT because PostgreSQL doesn't allow ORDER BY on columns not in DISTINCT list
-  # Must include all non-aggregated columns in GROUP BY for PostgreSQL
-  pages_data = Alchemy::Page
-    .joins("LEFT JOIN alchemy_users AS creators ON alchemy_pages.creator_id = creators.id")
-    .joins("LEFT JOIN alchemy_users AS updaters ON alchemy_pages.updater_id = updaters.id")
-    .joins("LEFT JOIN alchemy_pages AS replies ON alchemy_pages.id = replies.reply_target_id")
-    .select("alchemy_pages.id, creators.login AS creator_login, updaters.login AS updater_login, alchemy_pages.updated_at, STRING_AGG(replies.urlname, ', ' ORDER BY replies.created_at DESC) AS replied_by_urlnames")
-    .group("alchemy_pages.id, creators.login, updaters.login, alchemy_pages.updated_at")
-    .map do |page|
-      {
-        id: page.id,
-        created_by: page.creator_login || '',
-        last_updated_by: page.updater_login || '',
-        last_updated_date: page.updated_at&.strftime('%Y-%m-%d') || '',
-        replied_by: page.replied_by_urlnames || ''
-      }
-    end
-
-  max_length = [page_ids.length, user_ids.length, event_ids.length, topic_ids.length, profile_only_data.length, pages_data.length].max
+  max_length = [page_ids.length, user_ids.length, event_ids.length, topic_ids.length].max
 
   page_ids.fill("", page_ids.length...max_length)
   user_ids.fill("", user_ids.length...max_length)
   event_ids.fill("", event_ids.length...max_length)
   topic_ids.fill("", topic_ids.length...max_length)
-  profile_only_data.fill({id: '', updater_login: '', updated_at: ''}, profile_only_data.length...max_length)
-  pages_data.fill({id: '', created_by: '', last_updated_by: '', last_updated_date: '', replied_by: ''}, pages_data.length...max_length)
 
   page_ids.each_with_index do |page_id, index|
     report << {
@@ -89,15 +53,7 @@ def main(log_level = 'info')
       event_id: event_ids[index],
       profile_id: user_ids[index],
       themetag_id: topic_ids[index],
-      profile_only_id: profile_only_data[index][:id],
-      profile_only_last_updated_by: profile_only_data[index][:updater_login],
-      profile_only_last_updated_date: profile_only_data[index][:updated_at],
-      pages_id: pages_data[index][:id],
-      pages_created_by: pages_data[index][:created_by],
-      pages_last_updated_by: pages_data[index][:last_updated_by],
-      pages_last_updated_date: pages_data[index][:last_updated_date],
-      pages_replied_by: pages_data[index][:replied_by],
-  }
+    }
   end
 
   ############
